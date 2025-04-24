c80d77a461fc8bdf44028b3296cc8b809b2e5aad
Guru Prasad <155965731+guru-12345@users.noreply.github.com>
2025-04-24 10:52:09 +0530

    Create 5.2 ntroduction to coordinates.py

---
+++ 5.2 ntroduction to coordinates.py
@@ -0,0 +1,21 @@
+#Run the code and observe the x and y coordinates.
+#Task: Create a new `label3` with text 'bottom-right corner' and place it at the bottom-right.
+
+import customtkinter as ctk
+
+# Create the main window
+root = ctk.CTk()
+root.geometry("600x450")  # Window size 600x450
+
+# Adding a label at the top-left corner (0, 0)
+label1 = ctk.CTkLabel(root, text="Top-left Corner")
+label1.place(x=0, y=0)  #place() is used to position widgets at exact coordinates (x, y).
+
+# Adding a label at the center of the window (300, 225)
+label2 = ctk.CTkLabel(root, text="Center of the Window")
+label2.place(x=300, y=225)
+
+#Add the new label3 here
+
+# Run the application
+root.mainloop()
